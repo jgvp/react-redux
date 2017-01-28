ead0c05fdb292ec78a061bf50daaf5606d32f66e
Juan Felipe <juanfelipebedoyacardona@gmail.com>
2017-01-28 13:05:29 -0500

    Add firebase to store attendees

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
 import uuid from 'uuid';
 import Data from '../data/data';
+import * as firebase from 'firebase'
 
+
+const configFirebaseDevelop = {
+    apiKey: "AIzaSyDdMgLcISr4APnMe6v_tCdvGGUbj3KBxSk",
+    authDomain: "attendees-b5509.firebaseapp.com",
+    databaseURL: "https://attendees-b5509.firebaseio.com",
+    storageBucket: "attendees-b5509.appspot.com",
+    messagingSenderId: "882785827455"
+}
+
+firebase.initializeApp(configFirebaseDevelop);
 
 function fetchList () {
 	return new Promise((resolve, reject) => {
@@ -32,6 +43,13 @@ export const addListAttendees = () => (dispatch) =>  {
 }
 
 export const addAttendee = (name, color) => {
+	const attendee = {
+        id: uuid.v4(),
+        name: name,
+        color: color
+	}
+    firebase.database().ref('/attendees').push(attendee);
+
 	return {
 		type: 'ADD_ATTENDEE',
 		id: uuid.v4(),
